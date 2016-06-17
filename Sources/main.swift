4adc848086c648b5021ad37926382a78d0db63ab
Siemen Sikkema <siemen@unclutter.nl>
2016-06-17 15:54:32 +0200

    Add Xcode project

---
+++ Sources/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WhoWantsDownloadCommands
+//
+//  Created by Siemen Sikkema on 17/06/16.
+//  Copyright © 2016 Unclutter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
