7d4b3181ac961538efad7dc7f211df77dbd49c82
Siemen Sikkema <siemen@unclutter.nl>
2016-06-17 17:38:51 +0200

    Don't explicitly unwrap optionals

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,22 +1,47 @@
 import Kanna
 
-let url = URL(string: "https://developer.apple.com/videos/wwdc2016/")!
-let data = try! Data(contentsOf: url)
-
-guard let doc = Kanna.HTML(html: data, encoding: .utf8) else { fatalError() }
+guard let
+	url = URL(string: "https://developer.apple.com/videos/wwdc2016/"),
+	data = try? Data(contentsOf: url),
+	doc = Kanna.HTML(html: data, encoding: .utf8) else {
+		fatalError("Could not load WWDC page")
+}
 
 let videoImages = doc.xpath("//section[starts-with(@class, 'all-content')]//img[starts-with(@src, 'http://devstreaming.apple.com/videos/wwdc/2016/')]")
 
-for videoImage in videoImages {
-	let title = videoImage.at_xpath("@alt")!.text!
-	let src = videoImage.at_xpath("@src")!.text!
-	var components = URLComponents(string: src)!
-	let basePathComponents = components.path!.components(separatedBy: "/")[0...5]
-	let number = basePathComponents[5]
-	let videoPathComponents = basePathComponents + ["hls_vod_mvp.m3u8"]
-	let videoPath = videoPathComponents.joined(separator: "/")
-	components.path = videoPath
-	let videoURL = components.url!
+struct VideoLink {
+	let number: String
+	let title: String
+	let url: URL
 
-	print("ffmpeg -i \(videoURL) -map 0:6 -map 0:7 -c copy \"\(number) \(title).ts\"")
+	init?(xmlElement: XMLElement) {
+		guard
+			let title = xmlElement.at_xpath("@alt")?.text,
+			let src = xmlElement.at_xpath("@src")?.text,
+			var components = URLComponents(string: src),
+			let basePathComponents = components.path?.components(separatedBy: "/")[0...5] else {
+				return nil
+		}
+
+		let videoPathComponents = basePathComponents + ["hls_vod_mvp.m3u8"]
+		let videoPath = videoPathComponents.joined(separator: "/")
+
+		self.title = title
+		number = basePathComponents[5]
+
+		components.path = videoPath
+		if let url = components.url {
+			self.url = url
+		} else {
+			return nil
+		}
+	}
+}
+
+videoImages.flatMap(VideoLink.init).forEach {
+	let channels = [6, 7]
+	let channelMapString = channels
+		.map { "-map 0:\($0)" }
+		.joined(separator: " ")
+	print("ffmpeg -i \($0.url) \(channelMapString) -c copy \"\($0.number) \($0.title).ts\"")
 }
